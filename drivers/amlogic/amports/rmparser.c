17a4dbb33da418603694f4dd035361c35e40014e
Daniel Drake <drake@endlessm.com>
2015-05-26 11:49:12 -0600

    rmparser: fix build without corresponding decoder

--- drivers/amlogic/amports/rmparser.c
+++ drivers/amlogic/amports/rmparser.c
@@ -205,7 +205,7 @@ static ssize_t _rmparser_write(const char __user *buf, size_t count)
 			("write timeout,retry,halt_count=%d parse_control=%x\n",
 			 parse_halt, READ_MPEG_REG(PARSER_CONTROL));
 
-			vreal_set_fatal_flag(1);
+			//vreal_set_fatal_flag(1);
 
 			if (parse_halt > 10) {
 				WRITE_MPEG_REG(PARSER_CONTROL,
