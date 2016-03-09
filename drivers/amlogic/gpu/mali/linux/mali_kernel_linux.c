a526408b748e172c27e04b701c7113858b0f3afc
Carlo Caione <carlo@endlessm.com>
2016-03-09 18:44:25 +0100

    mali: Re-apply Amlogic's Mali changes
    Based on their changes to r5p1-01rel0 in gpu-2015-12-15-5f1e4e918c.tar.gz
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- drivers/amlogic/gpu/mali/linux/mali_kernel_linux.c
+++ drivers/amlogic/gpu/mali/linux/mali_kernel_linux.c
@@ -65,6 +65,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(mali_sw_counters);
 
 /* from the __malidrv_build_info.c file that is generated during build */
 extern const char *__malidrv_build_info(void);
+extern void mali_post_init(void);
+extern int mali_pdev_dts_init(struct platform_device* mali_gpu_device);
+extern int mpgpu_class_init(void);
+extern void mpgpu_class_exit(void);
 
 /* Module parameter to control log level */
 int mali_debug_level = 2;
@@ -371,7 +375,7 @@ int mali_module_init(void)
 	int err = 0;
 
 	MALI_DEBUG_PRINT(2, ("Inserting Mali v%d device driver. \n", _MALI_API_VERSION));
-	MALI_DEBUG_PRINT(2, ("Compiled: %s, time: %s.\n", __DATE__, __TIME__));
+//	MALI_DEBUG_PRINT(2, ("Compiled: %s, time: %s.\n", __DATE__, __TIME__));
 	MALI_DEBUG_PRINT(2, ("Driver revision: %s\n", SVN_REV_STRING));
 
 #if MALI_ENABLE_CPU_CYCLES
@@ -429,6 +433,8 @@ int mali_module_init(void)
 
 	MALI_PRINT(("Mali device driver loaded\n"));
 
+	mpgpu_class_init();
+
 	return 0; /* Success */
 }
 
@@ -458,6 +464,7 @@ void mali_module_exit(void)
 #if defined(CONFIG_MALI400_INTERNAL_PROFILING)
 	_mali_internal_profiling_term();
 #endif
+	mpgpu_class_exit();
 
 	MALI_PRINT(("Mali device driver unloaded\n"));
 }
@@ -495,6 +502,7 @@ static int mali_probe(struct platform_device *pdev)
 				err = mali_sysfs_register(mali_dev_name);
 
 				if (0 == err) {
+					mali_post_init();
 					MALI_DEBUG_PRINT(2, ("mali_probe(): Successfully initialized driver for platform device %s\n", pdev->name));
 
 					return 0;
