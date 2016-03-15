80e441519386b6a520b80c484bfb300a33fcb349
Carlo Caione <carlo@endlessm.com>
2016-03-15 16:43:27 +0100

    drm/meson: Temporarily disable CVBS support
    We had to disable CVBS support for several reasons:
    - We do not have yet supported modes for CVBS
    - All the code managing the switch is not used on our board
    - There is no more an AMLogic GPIO framework so we need to rewrite all
      the code using the mainline GPIO framework
    
    TODO: Fix CVBS support
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- drivers/gpu/drm/meson/meson_drv.c
+++ drivers/gpu/drm/meson/meson_drv.c
@@ -890,6 +890,7 @@ static int meson_load(struct drm_device *dev, unsigned long flags)
 		enabled_connectors = MESON_CONNECTORS_HDMI;
 
 	meson_hdmi_connector_create(dev, !!(enabled_connectors & MESON_CONNECTORS_HDMI));
+#if 0
 	meson_cvbs_init(dev);
 
 	{
@@ -909,6 +910,7 @@ static int meson_load(struct drm_device *dev, unsigned long flags)
 
 		meson_cvbs_connector_create(dev, !!(enabled_connectors & MESON_CONNECTORS_CVBS_PAL), mode);
 	}
+#endif
 
 	ret = drm_vblank_init(dev, dev->mode_config.num_crtc);
 	if (ret < 0) {
